40851f781aef0ad73aeb5507ee3c5fea0bb283b7
abdelrhman ibrahem <abdo.ibrahem1122@gmail.com>
2022-09-25 22:32:55 +0200

    Add static settings

--- config/urls.py
+++ config/urls.py
@@ -52,3 +52,6 @@ urlpatterns +=i18n_patterns(
         'redoc', cache_timeout=0), name='schema-redoc'),
 )
 
+
+urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
