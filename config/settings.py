4392fb7d587083b206562fa458201ca382fbd19f
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2023-06-22 19:07:19 +0300

    Update ip address

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ['*', '34.229.92.51']
+ALLOWED_HOSTS = ['*', '18.206.156.1']
 
 
 # Application definition
