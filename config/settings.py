fc0f7c7af78c8e71ae05976b932a938abb34f69a
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2023-06-22 18:52:37 +0300

    Update allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '34.229.92.51']
 
 
 # Application definition
